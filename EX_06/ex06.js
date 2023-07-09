52a510ae94de0d951d9298c1aab963f6633a29d3
Arsalan <arsalan.akkhter@gmail.com>
2023-07-09 21:09:55 +0500

    Finalizing ex_06

---
+++ EX_06/ex06.js
@@ -0,0 +1,3 @@
+var person_Name = "\t\t" + "Arsalan" + "\n" + "Akhtar" + "\t\t\t"; // Person's Name with white space characters
+console.log(person_Name); // with white space character
+console.log(person_Name.trim().replace(/[\r\n]/gm, '')); // without whitespace characters
