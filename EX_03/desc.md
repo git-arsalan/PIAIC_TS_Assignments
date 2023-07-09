66714bed50ad650be7fce392f8e5c9178ee3a81f
Arsalan <arsalan.akkhter@gmail.com>
2023-07-09 20:33:33 +0500

    finalizing ex04

--- EX_03/desc.md
+++ EX_03/desc.md
@@ -1,2 +1,2 @@
-## Objective for Excercise 2
+## Objective for Excercise 3
 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
---
+++ EX_04/ex04.js
@@ -0,0 +1,4 @@
+var famous_person = "Muhammad Ali Jinnah";
+var famous_Qoute = "Expect the best, prepare for the worst.";
+var output = famous_person + " Once said, " + '"' + famous_Qoute + '"';
+console.log(output);
