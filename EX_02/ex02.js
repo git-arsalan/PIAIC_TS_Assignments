56d2a84bc185fef35db142945f381802323e350e
Arsalan <arsalan.akkhter@gmail.com>
2023-07-09 14:48:46 +0500

    Completing exercise 02

---
+++ EX_02/ex02.js
@@ -0,0 +1,3 @@
+// creating some code in typescript
+var person_Name = "Arsalan Akhtar";
+console.log("Hello! " + person_Name + " would you like to learn some typescript today?");
---
+++ EX_02/ex02.ts
@@ -0,0 +1,3 @@
+// creating some code in typescript
+let person_Name = "Arsalan Akhtar";
+console.log("Hello! " + person_Name + " would you like to learn some typescript today?");
