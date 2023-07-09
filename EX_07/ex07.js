d6c1da3ebc9f748933f90e430ff193322c7bc0e2
Arsalan <arsalan.akkhter@gmail.com>
2023-07-09 21:30:31 +0500

    Finalizing exercise 07

---
+++ EX_07/ex07.js
@@ -0,0 +1,4 @@
+console.log(5 + 3); // Addition operation 
+console.log(10 - 2); // subtraction operation
+console.log(4 * 2); // multiplication operation
+console.log(16 / 2); // division operation
