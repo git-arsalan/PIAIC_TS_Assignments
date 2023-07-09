221cbf15ec68944930a00db447f021b4ed1f3f04
Arsalan <arsalan.akkhter@gmail.com>
2023-07-09 15:05:59 +0500

    Commitng ex 03

---
+++ EX_03/ex03.ts
@@ -0,0 +1,10 @@
+// code for exercise # 3
+let persons_Name = "arSalan akHtar"; // storing person's Name in a variable named as person_Name
+// printing person name in a lower case
+console.log(persons_Name.toLowerCase() + ": Person's Name in a lower case");
+// printing person name in a uppercase
+console.log(persons_Name.toUpperCase() + ": Person's Name in a upper case");
+// printing person name in title case
+console.log(persons_Name.to + ": Person's Name in a lower case");
+
+
