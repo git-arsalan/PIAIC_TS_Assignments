53a270bd7aada2c8675926d8b40f8baf9c108acc
ARSALAN <arsalan.akhtar@rayyanco.com>
2023-07-10 12:59:29 +0500

    finalizing ex12

---
+++ EX_12/ex12.js
@@ -0,0 +1,4 @@
+var guest_List = ["Ammar", "Yasir", "faysal"];
+console.log("".concat(guest_List[0], " you are invited for a dinner tonight"));
+console.log("".concat(guest_List[1], " you are invited for a dinner tonight"));
+console.log("".concat(guest_List[2], " you are invited for a dinner tonight"));
---
+++ EX_12/ex12.ts
@@ -0,0 +1,4 @@
+let guest_List = ["Ammar", "Yasir", "faysal"];
+console.log(`${guest_List[0]} you are invited for a dinner tonight`);
+console.log(`${guest_List[1]} you are invited for a dinner tonight`);
+console.log(`${guest_List[2]} you are invited for a dinner tonight`);
