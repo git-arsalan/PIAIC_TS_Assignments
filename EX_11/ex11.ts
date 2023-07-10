e7ba9ea7880bc13be4f86c0162e2359f10d532ba
ARSALAN <arsalan.akhtar@rayyanco.com>
2023-07-10 12:32:51 +0500

    Finalizing ex 11

---
+++ EX_11/ex11.ts
@@ -0,0 +1,4 @@
+let arr_transp_mode = ["MotorCyle" , "Car" , "Bus"];
+console.log(`${arr_transp_mode[0]} is the cheapest and fastest mode of transportation`);
+console.log(`${arr_transp_mode[1]} is the luxurious and faster mode of transportation`);
+console.log(`${arr_transp_mode[2]} is the best mode of transportation for common man`);
