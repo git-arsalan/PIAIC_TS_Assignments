e8c1f7de234f3f1e0e0c7840f127d30102a75661
Arsalan <arsalan.akkhter@gmail.com>
2023-07-10 16:39:13 +0500

    Finalizing ex13

---
+++ EX_13/ex13.ts
@@ -0,0 +1,9 @@
+var guest_List = ["Ammar", "Yasir", "faysal"];
+
+console.log("".concat(guest_List[1], " cannot meet us due some personal engagement"));
+console.log("We have a new guest!");
+console.log("below is the updated guest list!");
+guest_List[1] = "Arafat";
+console.log("".concat(guest_List[0], " you are invited for a dinner tonight"));
+console.log("".concat(guest_List[1], " you are invited for a dinner tonight"));
+console.log("".concat(guest_List[2], " you are invited for a dinner tonight"));
