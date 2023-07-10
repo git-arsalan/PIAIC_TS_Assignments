865a70f06649e0b763deb55247b188549b2b4e01
ARSALAN <arsalan.akhtar@rayyanco.com>
2023-07-10 12:19:57 +0500

    Finalizing Exercise 10

---
+++ EX_10/ex10.js
@@ -0,0 +1,6 @@
+var arr_myFriends = ["Ahmad", "Faraz", "Salman", "Nadir"];
+// Now accessing array elements
+console.log("Hello my dear friend ".concat(arr_myFriends[0])); // accesing 1st element with a personalized message
+console.log("Hello my dear friend ".concat(arr_myFriends[1])); // accessing 2nd element with a personalized message
+console.log("Hello my dear friend ".concat(arr_myFriends[2])); // accessing 3rd element with a personalized message
+console.log("Hello my dear friend ".concat(arr_myFriends[3])); // accessing 4th element with a personalized message
