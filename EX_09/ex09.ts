06b3f33dccff9572ae6ea90dc64ec59fe8428e5d
ARSALAN <arsalan.akhtar@rayyanco.com>
2023-07-10 12:12:05 +0500

    Finalizing ex09

---
+++ EX_09/ex09.ts
@@ -0,0 +1,6 @@
+let arr_myFriends = ["Ahmad", "Faraz" , "Salman", "Nadir"];
+// Now accessing array elements
+console.log(arr_myFriends[0]); // accesing 1st element
+console.log(arr_myFriends[1]); // accessing 2nd element
+console.log(arr_myFriends[2]); // accessing 3rd element
+console.log(arr_myFriends[3]); // accessing 4th element
