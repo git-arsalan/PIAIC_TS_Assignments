17570604b396385adab274049733937ef9c27984
Arsalan <arsalan.akkhter@gmail.com>
2023-07-13 12:28:51 +0500

    Finalizing ex21

---
+++ EX_21/ex21.js
@@ -0,0 +1,28 @@
+console.log("Following are the 5 correct conditional statements");
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); // 1st conditional statement
+var flower = 'Jasmine';
+console.log("Is flower == 'Jasmine'? I predict True."); // 
+console.log(flower == 'Jasmine'); // 2nd conditional statement
+var day = 'Thursday';
+console.log("Is day == 'Thursday'? I predict True.");
+console.log(day == 'Thursday'); // 3rd conditional statement
+var month = 'July';
+console.log("Is Month == 'July'? I predict True.");
+console.log(month == 'July'); // 4th conditional statement
+var Year = '2023';
+console.log("Is Year == '2023'? I predict True.");
+console.log(Year == '2023'); // 5th conditional statement
+// False conditional Statements
+console.log("Following are the 5 incorrect conditional statements");
+console.log("Is car != 'subaru'? I predict False.");
+console.log(car != 'subaru'); // 6th conditional statement
+console.log("Is flower != 'Jasmine'? I predict False."); // 
+console.log(flower != 'Jasmine'); // 7th conditional statement
+console.log("Is day != 'Thursday'? I predict False.");
+console.log(day != 'Thursday'); // 8th conditional statement
+console.log("Is Month != 'July'? I predict False.");
+console.log(month != 'July'); // 4th conditional statement
+console.log("Is Year != '2023'? I predict False.");
+console.log(Year != '2023'); // 5th conditional statement
---
+++ EX_21/ex21.ts
@@ -0,0 +1,48 @@
+console.log("Following are the 5 correct conditional statements");
+
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')  // 1st conditional statement
+
+let flower = 'Jasmine';
+console.log("Is flower == 'Jasmine'? I predict True.") // 
+console.log(flower == 'Jasmine')  // 2nd conditional statement
+
+let day = 'Thursday';
+console.log("Is day == 'Thursday'? I predict True.")
+console.log(day == 'Thursday')  // 3rd conditional statement
+
+let month = 'July';
+console.log("Is Month == 'July'? I predict True.")
+console.log(month == 'July')  // 4th conditional statement
+
+let Year = '2023';
+console.log("Is Year == '2023'? I predict True.")
+console.log(Year == '2023')  // 5th conditional statement
+
+// False conditional Statements
+
+console.log("Following are the 5 incorrect conditional statements");
+
+console.log("Is car != 'subaru'? I predict False.")
+console.log(car != 'subaru')  // 6th conditional statement
+
+console.log("Is flower != 'Jasmine'? I predict False.") // 
+console.log(flower != 'Jasmine')  // 7th conditional statement
+
+console.log("Is day != 'Thursday'? I predict False.")
+console.log(day != 'Thursday')  // 8th conditional statement
+
+console.log("Is Month != 'July'? I predict False.")
+console.log(month != 'July')  // 4th conditional statement
+
+console.log("Is Year != '2023'? I predict False.")
+console.log(Year != '2023')  // 5th conditional statement
+
+
+
+
+
+
+
+
