131b22d735b58301358255b436f661993ee24223
Arsalan <arsalan.akkhter@gmail.com>
2023-07-13 16:42:53 +0500

    finalizing Ex_25

---
+++ EX_25/ex25.js
@@ -0,0 +1,11 @@
+var alien_color = "red";
+console.log("Alien color:" + alien_color);
+if (alien_color == "green") {
+    console.log("You have just earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("You have just earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("You have just earned 15 points");
+}
