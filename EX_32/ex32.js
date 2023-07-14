e8420accdd1380d04437a924bb971a1974959b12
Arsalan <arsalan.akkhter@gmail.com>
2023-07-14 12:02:00 +0500

    Finalizing EX32

---
+++ EX_32/ex32.js
@@ -0,0 +1,7 @@
+var pizzas = ["Peproni", "Fajita", "BBQ"];
+// Loop through Array
+for (var pizza in pizzas) {
+    console.log("I like " + pizzas[pizza] + " Pizza");
+}
+// outside the loop
+console.log("I Really! Like Pizzas");
---
+++ EX_32/ex32.ts
@@ -0,0 +1,13 @@
+let pizzas = ["Peproni", "Fajita", "BBQ"];
+
+// Loop through Array
+
+for (var pizza in pizzas)
+{
+    console.log("I like " + pizzas[pizza] + " Pizza");
+
+}
+
+// outside the loop
+
+console.log ("I Really! Like Pizzas");
