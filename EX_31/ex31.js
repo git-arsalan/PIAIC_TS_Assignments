f7ed125188b687ed6366b6133f59f13c43223045
Arsalan <arsalan.akkhter@gmail.com>
2023-07-14 11:44:30 +0500

    Finalizing EX31

---
+++ EX_31/ex31.js
@@ -0,0 +1,18 @@
+// storing numbers in array
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// iterating through array
+console.log("Printing Ordinal Numbers!");
+for (var num in nums) {
+    if (nums[num] == 1) {
+        console.log("1st");
+    }
+    else if (nums[num] == 2) {
+        console.log("2nd");
+    }
+    else if (nums[num] == 3) {
+        console.log("3rd");
+    }
+    else {
+        console.log(nums[num] + "th");
+    }
+}
