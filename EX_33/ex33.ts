380483b9f15e24d30aa3da950d3c7c8952f314ee
Arsalan <arsalan.akkhter@gmail.com>
2023-07-14 12:23:00 +0500

    Finalizing Ex33

---
+++ EX_33/ex33.ts
@@ -0,0 +1,29 @@
+// Creating an array
+let animals_array = ["Cow", "Hen", "Dog"];
+
+// Loop through the array
+
+for (var animal in animals_array)
+{
+    if (animals_array[animal] == "Cow")
+    {
+        console.log("Cow provide us milk");
+
+    }
+    if (animals_array[animal] == "Hen")
+    {
+        console.log("Hen provide us Eggs");
+
+    }
+    if (animals_array[animal] == "Dog")
+    {
+        console.log("Dogs are kept for security purposes");
+
+    }
+  
+
+}
+
+// Final Comment
+
+console.log("All these Animals are usefull domestic Animals");
