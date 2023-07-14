da7306722e410488f74a88a84954b96878e29f95
Arsalan <arsalan.akkhter@gmail.com>
2023-07-14 14:21:13 +0500

    Finalizing EX34

---
+++ EX_34/ex34.ts
@@ -0,0 +1,16 @@
+// Creating a function
+
+function Make_TShirt(size:string, message:string)
+{
+    console.log("The size of Shirt is " + size);
+    console.log("The message printed on shirt is :" + message);
+
+}
+
+// call the above function
+
+console.log("Before function call");
+
+Make_TShirt("Large","Pakistan Zindabad!");
+
+console.log("After function call");
